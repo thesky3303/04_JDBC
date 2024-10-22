bd837dd11ed650039d5423795fe6cdc744263d7d
thesky3303 <thesky3303@gmail.com>
2024-10-22 14:46:09 +0900

    1022

--- jdbc2/src/edu/kh/jdbc/dao/UserDAO.java
+++ jdbc2/src/edu/kh/jdbc/dao/UserDAO.java
@@ -1,5 +1,439 @@
 package edu.kh.jdbc.dao;
 
+import static edu.kh.jdbc.common.JDBCTemplate.*;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.kh.jdbc.common.JDBCTemplate;
+import edu.kh.jdbc.dto.User;
+
+
+//DAO (Data Access Object)
+//데이터가 저장된 곳에 접근하는 용도의 객체
+//-> DB에 접근하여 Java에서 원하는 결과를 얻기위해
+// sql을 수행하고 결과를 반환 받는 역할
 public class UserDAO {
+	
+	// 필드
+	// - DB 접근 관련한 JDBC 객체 참조 변수 미리 선언
+	private Statement stmt = null;
+	private PreparedStatement pstmt = null;
+	private ResultSet rs = null;
+
+	/** 전달 받은 Connection을 이용해서 DB에 접근하여
+	 * 전달 받은 아이디와 일치하는 User 정보 조회하기
+	 * @param conn : Service에서 생성한 Connection 객체
+	 * @param input : View에서 입력받은 아이디
+	 * @return 아이디가 일치하는 회원의 User 또는 null
+	 */
+	public User selectId(Connection conn, String input) {
+		
+		User user = null; // 결과 저장용 변수
+		
+		try {
+			
+			// SQL 작성
+			String sql = "SELECT * FROM TB_USER WHERE USER_ID = ?";
+			
+			// PreparedStatement 객체 생성
+			pstmt = conn.prepareStatement(sql);
+			
+			// ?(위치 홀더)에 알맞은 값 대입
+			pstmt.setString(1, input);
+			
+			// SQL 수행 후 결과 반환 받기
+			rs = pstmt.executeQuery();
+			
+			
+			// 조회 결과가 있을 경우
+			// -> 중복되는 아이디가 없을 경우
+			//	1행만 조회되기 때문에 while문 보다는 if를 사용하는게 효과적
+			if(rs.next()) {
+				// 첫 행에 데이터가 존재한다면
+				
+				// 각 컬럼의 값 얻어오기
+				int userNo = rs.getInt("USER_NO");
+				String userId = rs.getString("USER_ID");
+				String userPw = rs.getString("USER_PW");
+				String userName = rs.getString("USER_NAME");
+				
+				// java.sql.Date 활용
+				Date enrollDate = rs.getDate("ENROLL_DATE");
+				
+				// 조회된 컬럼값을 이용해서 User 객체 생성
+				user = new User(userNo, 
+								userId,
+								userPw,
+								userName,
+								enrollDate.toString());
+				
+			}
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			
+		} finally {
+			// 사용한 JDBC 객체 자원 반환(close)
+			JDBCTemplate.close(rs);
+			JDBCTemplate.close(pstmt);
+			
+			// Connection 객체는 생성된 Service에서 close!!!
+		}
+		
+		return user; // 결과 반환( 생성된 User 또는 null )
+	}
+
+	/** User 등록 DAO 메서드
+	 * @param conn : DB 연결 정보가 담겨있는 Connection 객체
+	 * @param user : 입력받은 id,pw,name이 세팅된 User 객체
+	 * @return INSERT 결과 행의 개수
+	 */
+	public int insertUser(Connection conn, User user) throws Exception{
+		
+		// SQL 수행 중 발생하는 예외를
+		// catch로 처리하지 않고, throws를 이용해서 호출부로 던져 처리
+		// -> catch문 필요 없다!
+		
+		// 1. 결과 저장용 변수 선언
+		int result = 0;
+		
+		try {
+			
+			// 2. SQL 작성
+			String sql = """
+					INSERT INTO TB_USER
+					VALUES(SEQ_USER_NO.NEXTVAL, ?, ?, ?, DEFAULT)""";
+			
+			// 3. PreparedStatement 생성
+			pstmt = conn.prepareStatement(sql);
+			
+			// 4. ?(위치홀더) 알맞은 값 대입
+			pstmt.setString(1, user.getUserId());
+			pstmt.setString(2, user.getUserPw());
+			pstmt.setString(3, user.getUserName());
+			
+			// 5. SQL(INSERT) 수행(executeUpdate()) 후 결과(삽입된 행의 개수) 반환받기
+			result = pstmt.executeUpdate();
+			
+			
+		} finally {
+			// 6. 사용한 jdbc 객체 자원 반환(close)
+			close(pstmt);
+			
+		}
+		
+		// 결과 저장용 변수에 저장된 값 반환
+		return result;
+	}
+
+	/** User 전체 조회 DAO 메서드
+	 * @param conn 
+	 * @return userList
+	 */
+	public List<User> selectAll(Connection conn) throws Exception {
+		
+		// 1. 결과 저장용 변수 선언
+		// -> List 같은 컬렉션을 반환하는 경우에는
+		//    변수 선언 시 객체도 같이 생성해두자!
+		List<User> userList = new ArrayList<User>();
+		
+		try {
+			
+			// 2. SQL 작성
+			String sql = """
+					SELECT USER_NO, USER_ID, USER_PW, USER_NAME, 
+					TO_CHAR(ENROLL_DATE, 'YYYY"년" MM"월" DD"일"') ENROLL_DATE
+					FROM TB_USER
+					ORDER BY USER_NO ASC
+					""";
+			
+			// 3. PreparedStatement 생성
+			pstmt = conn.prepareStatement(sql);
+			
+			// 4. ? 에 알맞은 값 대입 (없으면 패스)
+			
+			// 5. SQL(SELECT) 수행(executeQuery()) 후 
+			// 결과 반환(ResultSet) 받기
+			rs = pstmt.executeQuery();
+			
+			// 6. 조회 결과를 1행씩 접근하여 컬럼 값 얻어오기
+			
+			// 몇 행이 조회될지 모른다 -> while
+			// 무조건 1행이 조회된다   -> if
+			
+			while(rs.next()) {
+				
+				int userNo = rs.getInt("USER_NO");
+				String userId = rs.getString("USER_ID");
+				String userPw = rs.getString("USER_PW");
+				String userName = rs.getString("USER_NAME");
+				String enrollDate = rs.getString("ENROLL_DATE");
+				// - java.sql.Date 타입으로 값을 저장하지 않은 이유
+				//  -> TO_CHAR() 를 이용해서 문자열로 변환했기 때문!
+				
+				// userList 에 추가
+				// -> User 객체를 생성해 조회된 값을 담고
+				//  userList에 추가하기!
+				User user = new User(userNo, userId, userPw, userName, enrollDate);
+				
+				userList.add(user);
+				
+			}
+			
+			
+			
+		} finally {
+			close(rs);
+			close(pstmt);
+		}
+		
+		// 조회 결과가 담긴 List 반환
+		return userList;
+	}
+
+	/** 이름에 검색어가 포함되는 회원 모두 조회 DAO
+	 * @param conn
+	 * @param keyword
+	 * @return searchList
+	 */
+	public List<User> selectName(Connection conn, String keyword) throws Exception {
+		
+		// 1. 결과 저장용 변수 선언
+		List<User> searchList = new ArrayList<User>();
+		
+		try {
+			// 2. SQL 작성
+			String sql = """
+					SELECT USER_NO, USER_ID, USER_PW, USER_NAME, 
+					TO_CHAR(ENROLL_DATE, 'YYYY"년" MM"월" DD"일"') ENROLL_DATE
+					FROM TB_USER
+					WHERE USER_NAME LIKE '%' || ? || '%'
+					ORDER BY USER_NO ASC
+					""";
+			
+			
+			// 3. PreparedStatement 객체 생성
+			pstmt = conn.prepareStatement(sql);
+			
+			// 4. ? 에 알맞은 값 대입하기
+			pstmt.setString(1, keyword);
+			
+			// 5. DB 수행 후 결과 반환받기
+			rs = pstmt.executeQuery();
+			
+			while(rs.next()) {
+				
+				int 	userNo 	   = rs.getInt("USER_NO");
+				String  userId 	   = rs.getString("USER_ID");
+				String  userPw     = rs.getString("USER_PW");
+				String  userName   = rs.getString("USER_NAME");
+				String  enrollDate = rs.getString("ENROLL_DATE");
+				
+				User user = new User(userNo, userId, userPw, userName, enrollDate);
+				
+				searchList.add(user);
+				
+			}
+			
+		} finally {
+			close(rs);
+			close(pstmt);
+		}
+		
+		return searchList;
+	}
+
+	/** 4. USER_NO를 입력 받아 일치하는 User 조회 DAO
+	 * @param conn
+	 * @param input
+	 * @return user 객체 or null
+	 */
+	public User selectUser(Connection conn, int input) throws Exception{
+		
+		// 1. 결과 저장용 변수 선언
+		User user = null;
+		
+		try {
+			// 2. SQL 작성
+			String sql = """
+					SELECT USER_NO, USER_ID, USER_PW, USER_NAME, 
+					TO_CHAR(ENROLL_DATE, 'YYYY"년" MM"월" DD"일"') ENROLL_DATE
+					FROM TB_USER
+					WHERE USER_NO = ?
+					""";
+			
+			pstmt = conn.prepareStatement(sql);
+			
+			pstmt.setInt(1, input);
+			
+			rs = pstmt.executeQuery();
+			
+			if(rs.next()) {
+				int 	userNo 	   = rs.getInt("USER_NO");
+				String  userId 	   = rs.getString("USER_ID");
+				String  userPw     = rs.getString("USER_PW");
+				String  userName   = rs.getString("USER_NAME");
+				String  enrollDate = rs.getString("ENROLL_DATE");
+				
+				user = new User(userNo, userId, userPw, userName, enrollDate);
+			}
+			
+			
+		} finally {
+			close(rs);
+			close(pstmt);
+		}
+		
+		return user;
+	}
+
+	/** USER_NO를 입력 받아 일치하는 User 삭제 DAO
+	 * @param conn
+	 * @param input
+	 * @return result
+	 */
+	public int deleteUser(Connection conn, int input) throws Exception {
+		
+		int result = 0;
+		
+		try {
+			
+			String sql = """
+					DELETE FROM TB_USER
+					WHERE USER_NO = ?
+					""";
+			
+			pstmt = conn.prepareStatement(sql);
+			
+			pstmt.setInt(1, input);
+			
+			result = pstmt.executeUpdate();
+			
+			
+		} finally {
+			close(pstmt);
+		}
+		
+		return result;
+	}
+
+	/**  ID, PW가 일치하는 회원의 USER_NO 조회 DAO
+	 * @param conn
+	 * @param userId
+	 * @param userPw
+	 * @return userNo
+	 */
+	public int selectUser(Connection conn, String userId, String userPw) throws Exception{
+		
+		int userNo = 0; // 결과 저장용 변수 선언
+		
+		try {
+			
+			String sql = """
+					SELECT USER_NO
+					FROM TB_USER
+					WHERE USER_ID = ?
+					AND   USER_PW = ?
+					""";
+			
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, userId);
+			pstmt.setString(2, userPw);
+			
+			rs = pstmt.executeQuery();
+			
+			// 조회된 1행이 있을 경우
+			if(rs.next()) {
+				userNo = rs.getInt("USER_NO");
+			}
+			
+			
+		} finally {
+			close(rs);
+			close(pstmt);
+		}
+		
+		return userNo; // 조회 성공 USER_NO, 실패 0 반환
+	}
+
+	/** userNo가 일치하는 User의 이름 수정 DAO
+	 * @param conn
+	 * @param userName
+	 * @param userNo
+	 * @return result
+	 */
+	public int updateName(Connection conn, String userName, int userNo) throws Exception{
+		
+		int result = 0;
+		
+		try {
+			String sql = """
+					UPDATE TB_USER
+					SET USER_NAME = ?
+					WHERE USER_NO = ?""";
+			
+			pstmt = conn.prepareStatement(sql);
+			
+			pstmt.setString(1, userName);
+			pstmt.setInt(2, userNo);
+			
+			result = pstmt.executeUpdate();
+			
+		} finally {
+			close(pstmt);
+		}
+		
+		return result;
+	}
+
+	/** 아이디 중복 확인 DAO
+	 * @param conn
+	 * @param userId
+	 * @return
+	 */
+	public int idCheck(Connection conn, String userId) throws Exception {
+
+		int count = 0;
+		
+		try {
+			String sql = """
+					SELECT COUNT(*) 
+					FROM TB_USER
+					WHERE USER_ID = ?
+					""";
+			
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, userId);
+			
+			rs = pstmt.executeQuery();
+			
+			if(rs.next()) {
+				count = rs.getInt(1); // 조회된 컬럼 순서를 이용해
+								// 컬럼값 얻어오기
+			}
+			
+			
+		} finally {
+			close(rs);
+			close(pstmt);
+		}
+		
+		return count;
+	}
+	
+	
+	
+	
+	
+	
+	
 
 }
+
--- jdbc2/src/edu/kh/jdbc/service/UserService.java
+++ jdbc2/src/edu/kh/jdbc/service/UserService.java
@@ -1,5 +1,265 @@
 package edu.kh.jdbc.service;
 
-public class UserService {
+import static edu.kh.jdbc.common.JDBCTemplate.*;
 
+// import static : 지정된 경로에 존재하는 static 구문을 모두 얻어와
+// 클래스명.메서드명() 이 아닌 메서드명() 만 작성해도 호출 가능하게 함
+
+import java.sql.Connection;
+import java.util.List;
+
+import edu.kh.jdbc.common.JDBCTemplate;
+import edu.kh.jdbc.dao.UserDAO;
+import edu.kh.jdbc.dto.User;
+
+public class UserService {
+	
+	// 필드
+	private UserDAO dao = new UserDAO();
+
+	/** 전달 받은 아이디와 일치하는 User 정보 반환 서비스
+	 * @param input (입력된 아이디)
+	 * @return 아이디가 일치하는 회원 정보, 없으면 null
+	 */
+	public User selectId(String input) {
+		
+		// 커넥션 생성
+		Connection conn = JDBCTemplate.getConnction();
+		
+		// DAO 메서드 호출 후 결과 반환 받기
+		User user = dao.selectId(conn, input);
+		
+		// 다 쓴 커넥션 닫기
+		JDBCTemplate.close(conn);
+		
+		
+		return user; // DB 조회 결과 반환
+	}
+
+	/** User 등록 서비스
+	 * @param user : 입력 받은 id,pw,name이 세팅된 객체
+	 * @return 삽입 성공한 결과 행의 개수
+	 */
+	public int insertUser(User user) throws Exception{
+		
+		// 1. 커넥션 생성
+		Connection conn = JDBCTemplate.getConnction();
+		
+		// 2. 데이터 가공(할게 없으면 넘어감)
+		
+		// 3. DAO 메서드 호출(INSERT) 후 
+		// 결과(삽입 성공한 행 개수, int) 반환 받기
+		int result = dao.insertUser(conn, user);
+		
+		// 4. INSERT 수행 결과에 따라 트랜잭션 제어 처리
+		if(result > 0) { // INSERT 성공
+			commit(conn);
+			
+		} else { // INSERT 실패
+			rollback(conn);
+			
+		}
+		
+		// 5. Connection 반환하기
+		close(conn);
+		
+		// 6. 결과 반환
+		return result;
+	}
+
+	/** User 전체 조회 서비스
+	 * @return 조회된 User가 담긴 List
+	 */
+	public List<User> selectAll() throws Exception{
+		
+		// 1. 커넥션 생성
+		Connection conn = JDBCTemplate.getConnction();
+		
+		// 2. 데이터 가공(없으면 넘어감)
+		
+		// 3. DAO 메서드 호출(SELECT) 후 결과 반환(List<User>)받기
+		List<User> userList = dao.selectAll(conn);
+		
+		// 4. DML 인 경우 트랜잭션 처리
+		//    SELECT 는 안해도 된다!
+		
+		// 5. Connection 반환
+		close(conn);
+		
+		// 6. 결과 반환
+		return userList;
+	}
+
+	/** 3. User 중 이름에 검색어가 포함된 회원 조회 서비스
+	 * @param keyword : 입력한 키워드
+	 * @return searchList : 조회된 회원 리스트
+	 */
+	public List<User> selectName(String keyword) throws Exception {
+		
+		// 1. 커넥션 생성
+		Connection conn = JDBCTemplate.getConnction();
+		
+		// 2. 데이터 가공(없으면 패스)
+		
+		// 3. DAO 메서드 호출 후 결과 반환받기
+		List<User> searchList = dao.selectName(conn, keyword);
+		
+		// 4. DML인 경우 트랜잭션 처리(SELECT 패스)
+		
+		// 5. 커넥션 반환
+		close(conn);
+		
+		// 6. 결과 반환
+		return searchList;
+	}
+
+	/** 4. USER_NO를 입력 받아 일치하는 User 조회 서비스
+	 * @param input 입력한 사용자번호
+	 * @return User (조회된 회원 정보 객체 또는 null)
+	 */
+	public User selectUser(int input) throws Exception {
+	
+		// 1. 커넥션 생성
+		Connection conn = JDBCTemplate.getConnction();
+		
+		// 2. dao 호출 결과반환
+		User user = dao.selectUser(conn, input);
+		
+		// 3. 커넥션 반환
+		close(conn);
+		
+		// 4. 결과 반환
+		return user;
+	}
+
+	/** USER_NO를 입력 받아 일치하는 User 삭제 서비스
+	 * @param input
+	 * @return result
+	 */
+	public int deleteUser(int input) throws Exception {
+		
+		Connection conn = JDBCTemplate.getConnction();
+		
+		int result = dao.deleteUser(conn, input);
+		
+		// 결과에 따라 트랜잭션 제어 처리
+		if(result > 0) {
+			commit(conn);
+		} else {
+			rollback(conn);
+		}
+		
+		close(conn);
+		
+		return result;
+	}
+
+	/** ID, PW가 일치하는 회원의 USER_NO 조회
+	 * @param userId
+	 * @param userPw
+	 * @return userNo
+	 */
+	public int selectUserNo(String userId, String userPw) throws Exception{
+		
+		Connection conn = JDBCTemplate.getConnction(); // 커넥션 생성
+		
+		// DAO 호출 후 결과 반환 받기
+		int userNo = dao.selectUser(conn, userId, userPw);
+		
+		close(conn); // 커넥션 반환
+		
+		return userNo; // 결과 반환
+	}
+
+	/** userNo가 일치하는 User의 이름 수정 서비스
+	 * @param userName
+	 * @param userNo
+	 * @return result
+	 * @throws Exception
+	 */
+	public int updateName(String userName, int userNo) throws Exception{
+		
+		Connection conn = JDBCTemplate.getConnction();
+		
+		int result = dao.updateName(conn, userName, userNo);
+		
+		// 트랜잭션 제어
+		if(result > 0) commit(conn);
+		else		   rollback(conn);
+		
+		close(conn);
+		
+		return result;
+	}
+
+	public int idCheck(String userId) throws Exception {
+		
+		Connection conn = getConnction();
+		
+		int count = dao.idCheck(conn, userId);
+		
+		close(conn);
+		
+		return count;
+	}
+
+	/** userList에 있는 모든 user INSERT 서비스
+	 * @param userList
+	 * @return result : 삽입된 행의 개수
+	 */
+	public int multiInsertUser(List<User> userList) throws Exception {
+		
+		Connection conn = getConnction();
+		
+		// 다중 INSERT 방법
+		// 1) SQL 을 이용한 다중 INSERT
+		// 2) Java 반복문을 이용한 다중 INSERT (이거 사용!)
+		
+		int count = 0 ; // 삽입 성공한 행의 개수 count
+		
+		// 1행씩 삽입
+		for(User user : userList) {
+			int result = dao.insertUser(conn, user);
+			count += result; // 삽입 성공한 행의 개수를 count에 누적
+		}
+		
+		//count --; // 강제 실패
+		
+		// 트랜잭션 제어 처리
+		// 전체 삽입 성공 시 commit / 아니면 rollback(일부 삽입, 전체 실패)
+		if(count == userList.size()) {
+			commit(conn);
+		} else {
+			rollback(conn);
+		}
+		close(conn);
+		
+		return count;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
